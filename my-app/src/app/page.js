33d1fd0622396cd122892bcd28e23a66e9536987
Aron <81845582+unratedjanix@users.noreply.github.com>
2026-04-19 18:41:39 -0500

    style home page

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,14 +1,19 @@
+import Link from "next/link";
+import "./styles/home_page_styles.css";
 
 export default function Home() {
   return (
-   <div>
-    <h1 id="Home_Welcome_Header"> Welcome to Nullify</h1>
-    <p id="Information_para">Store your data safely</p>
+    <div>
+      <h1 id="Home_Welcome_Header">Welcome to Nullify</h1>
+      <p id="Information_para">Store your data safely</p>
 
-    <button className="Login_btn" > Login </button> 
+      <Link href="/login">
+        <button className="Login_btn">Login</button>
+      </Link>
 
-    <button className="Signup_btn">Sign in</button>
-
-   </div>
+      <Link href="/signup">
+        <button className="Signup_btn">Sign up</button>
+      </Link>
+    </div>
   );
 }
