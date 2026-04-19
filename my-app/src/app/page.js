96f2c1a418f8f876fdd57c04863c651c82f9430e
Aron <81845582+unratedjanix@users.noreply.github.com>
2026-04-19 18:19:16 -0500

    add my-app project

---
+++ my-app/src/app/page.js
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+   <div>
+    <h1 id="Home_Welcome_Header"> Welcome to Nullify</h1>
+    <p id="Information_para">Store your data safely</p>
+
+    <button className="Login_btn" > Login </button> 
+
+    <button className="Signup_btn">Sign in</button>
+
+   </div>
+  );
+}
