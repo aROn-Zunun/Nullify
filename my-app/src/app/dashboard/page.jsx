c8d02fea54aef6bca6e026b44d102da31b984c00
az <az@azs-Mac-mini.local>
2026-04-26 03:23:08 -0500

    Total count of files uploaded working

--- my-app/src/app/dashboard/page.jsx
+++ my-app/src/app/dashboard/page.jsx
@@ -1,15 +1,31 @@
 "use client";
 import "../styles/dashboard_styles.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
 
+
 export default function Dashboard() {
+  const [count, setCount]=useState(0)
   const router = useRouter();
 
+  useEffect(()=> {
+    async function fetchDashboard(){ 
+      
+      const response= await fetch('/api/dashboard')
+      const data = await response.json()
+
+      
+      setCount(data.file_count)
+    }
+      fetchDashboard()
+    },[])
+  
   return (
     <div id="dashboard_container">
       <h1 id="dashboard_header">Dashboard</h1>
       <p id="dashboard_description">Welcome to your dashboard!</p>
+      <h1 id="total_uploads_title">Total Uploads</h1>
+      <p id="count_uploads">You have uploaded: {count} amount of files!</p>
     </div>
   );
 }
